253cf3e4cb3922b58655cab1a53fc6d7f7f6e5c5
Willow Ciesialka <wciesialka@gmail.com>
2025-03-20 18:32:05 -0500

    Added more documentation.

--- src/wordscramble/__main__.py
+++ src/wordscramble/__main__.py
@@ -2,19 +2,25 @@
 import wordscramble.cli as cli
 
 def main():
+    # Create our CLI handler.
     scrambler = cli.WordScrambleCLI()
-    scrambler.intro()
+    scrambler.perform_intro()
+    # Let the handler loose! Make it start giving the user menus!
     while True:
         message = scrambler.scramble_menu() 
+        # If there's a message to report, send it!
         if message:
             print(message)
 
 if __name__ == "__main__":
     try:
         main()
+    # Allows us to press CTRL-C to quit without an annoying message.
     except KeyboardInterrupt:
         pass
-    except cli.UserQuitException:
+    # Check to see if the User asked to quit.
+    except cli.MenuExit:
         print("Goodbye!")
+    # Report any other errors.
     except Exception as ex:
         raise
--- src/wordscramble/cli.py
+++ src/wordscramble/cli.py
@@ -1,44 +1,52 @@
 # Import our command-line interface module.
 import wordscramble.menus as menus
-# Import our key-generation module
-import wordscramble.keygen as keygen
 # Import our word-scrambling module
 import wordscramble.scramble as scramble
-# Import deepcopy to make "deep copies" of dictionaries
-from copy import deepcopy
+# Import our key class from our key module
+from wordscramble.key import Key
 
-class UserQuitException(SystemExit):
+# Create a custom Exception so we can quit from menus anywhere.
+class MenuExit(SystemExit):
     def __init__(self):
         super().__init__()
         self.code = 0
 
+# Create a class to handle Word Scramble CLI functionality.
 class WordScrambleCLI:
 
     def __init__(self):
-        self.__key = None
+        self.__key: Key = None
     
     @property
-    def key(self):
-        return deepcopy(self.__key)
+    def key(self) -> Key:
+        return self.__key
     
-    def has_key(self):
+    # Check to see if we have a key already. If we do, return True. Otherwise,
+    # return False.
+    def has_key(self) -> bool:
         return not (self.__key is None)
 
+    # Ask the user if they'd like to generate a new key or import a key,
+    # then set whichever they choose to our key.
     def get_key(self):
         choice = menus.key_menu()
         if choice == menus.KeyMenuChoice.GENERATE_NEW_KEY:
-            self.__key = keygen.generate_random_key()
+            self.__key = Key.random()
         elif choice == menus.KeyMenuChoice.IMPORT_KEY:
             key_string = input("Your key: ")
-            self.__key = keygen.decode_key(key_string)
+            self.__key = Key.loads(key_string)
         elif choice == menus.KeyMenuChoice.QUIT:
-            raise UserQuitException
+            raise MenuExit
     
-    def scramble_menu(self):
+    # Show the user the main menu.
+    def scramble_menu(self) -> str:
+        # Set a key if we don't have one already.
         if not self.has_key():
             print("No key currently loaded.")
             self.get_key()
+        # Show the user the menu.
         choice = menus.scramble_menu()
+        # Parse user choice.
         if choice == menus.ScrambleMenuChoice.SCRAMBLE_MESSAGE:
             word = menus.get_user_message()
             scrambled = scramble.scramble(word, self.key)
@@ -48,14 +56,14 @@ class WordScrambleCLI:
             unscrambled = scramble.unscramble(word, self.key)
             return f"Your unscrambled message: \"{unscrambled}\""
         elif choice == menus.ScrambleMenuChoice.EXPORT_KEY:
-            key_string = keygen.encode_key(self.key)
+            key_string = self.key.dumps()
             return f"Your key: \"{key_string}\""
         elif choice == menus.ScrambleMenuChoice.NEW_KEY:
             self.__key = self.get_key()
             return f"New key set."
         elif choice == menus.ScrambleMenuChoice.QUIT:
-            raise UserQuitException
+            raise MenuExit
     
-    def intro(self):
+    def perform_intro(self):
         intro_message = "== Welcome to Word Scrambler! =="
         print(f"{intro_message:^80}")
